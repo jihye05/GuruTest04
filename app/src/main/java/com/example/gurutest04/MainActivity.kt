ccfa6ddde4e05be68b10ce7c8314c85866e672f4
jihye05 <gosjh1105@naver.com>
2022-07-25 11:15:00 +0900

    Start, End Time commit

---
+++ app/src/main/java/com/example/gurutest04/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.gurutest04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mPickTimeBtn = findViewById<Button>(R.id.StartTimeBtn)
+        val ePickTimeBtn = findViewById<Button>(R.id.EndTimeBtn)
+        val starttextView     = findViewById<TextView>(R.id.timeStart)
+        val endtextView     = findViewById<TextView>(R.id.timeEnd)
+
+        //시작시간 버튼
+        mPickTimeBtn.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+                starttextView.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        }
+
+        //끝나는 시간 버튼
+        ePickTimeBtn.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+                endtextView.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        }
+
+    }
+}
